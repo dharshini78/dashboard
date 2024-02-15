d84c3bed831fe372c6d1d2f1b2d3ae834c030c45
Dharshini R <soundaryaraja48@gmail.com>
2024-02-16 00:20:28 +0530

    added ipfs

--- src/pages/Notes.js
+++ src/pages/Notes.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { MultipleFilesUpload, TextUpload } from 'react-ipfs-uploader';
+import './Notes.css'
 
 const Notes = () => {
-  return (
-    <div>Notes</div>
-  )
-}
+    const [multipleFilesUrl, setMultipleFilesUrl] = useState('');
+    const [textUrl, setTextUrl] = useState('');
 
-export default Notes
+    return (
+        <div className='uploads'>
+            <div className='multiple'>
+                <MultipleFilesUpload setUrl={setMultipleFilesUrl} />
+                MultipleFilesUrl : 
+                <a
+                    href={multipleFilesUrl}
+                    target='_blank'
+                    rel='noopener noreferrer'
+                >
+                    {multipleFilesUrl}
+                </a>
+            </div>
+
+            <div className='text'>
+                <TextUpload setUrl={setTextUrl} />
+                TextUrl : 
+                <a
+                    href={textUrl}
+                    target='_blank'
+                    rel='noopener noreferrer'
+                >
+                    {textUrl}
+                </a>
+            </div>
+        </div>
+    );
+};
+
+export default Notes;
